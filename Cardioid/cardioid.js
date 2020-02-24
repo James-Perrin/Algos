af6c1d6c95e18841d038925dbca443703a31e763
James-Perrin <j.perrin2116@gmail.com>
2020-02-24 00:24:14 -0500

    created elements

--- Cardioid/cardioid.js
+++ Cardioid/cardioid.js
@@ -1,33 +1,67 @@
-let r;
-let factor = 0;
+const CIRCLE_DIAMETER = 125;
+const r = CIRCLE_DIAMETER / 2;
+
+const minBase = 0;
+const finalBase = 50;
+
+const minFactor = 0;
+const finalFactor = finalBase;
+
+const hieght = 1.10 * (finalBase - minBase) * CIRCLE_DIAMETER + 2*CIRCLE_DIAMETER;
+const width = 1.10 * (finalFactor - minFactor) * CIRCLE_DIAMETER + 2*CIRCLE_DIAMETER;
+
+let factor = minFactor;
+let base = minBase;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  r = height / 2 - 16;
+  createCanvas(width, hieght);
+  background(0);
+  stroke(255, 255, 255);
+  strokeWeight(1);
+  noFill();
 }
 
-function getVector(index, total) {
-  const angle = map(index % total, 0, total, 0, TWO_PI);
+function getVector(index, base) {
+  const angle = map(index % base, 0, base, 0, TWO_PI);
   const v = p5.Vector.fromAngle(angle + PI);
   v.mult(r);
   return v;
 }
 
 function draw() {
-  background(0);
-  const total = 200; //int(map(mouseX, 0, width, 0, 200));
-  factor += 0.02;
-
-  translate(width / 2, height / 2);
-  stroke(255, 150);
-  strokeWeight(2);
-  noFill();
-  ellipse(0, 0, r * 2);
-
-  strokeWeight(2);
-  for (let i = 0; i < total; i++) {
-    const a = getVector(i, total);
-    const b = getVector(i * factor, total);
-    line(a.x, a.y, b.x, b.y);
+  if (factor <= base) {
+    translate(CIRCLE_DIAMETER + (factor-minFactor) * 1.10 * CIRCLE_DIAMETER, CIRCLE_DIAMETER + (base-minBase) * 1.10 *  CIRCLE_DIAMETER);
+    ellipse(0, 0, r * 2);
+    for (let i = 0; i <= base; i++) {
+      const a = getVector(i, base);
+      const b = getVector(i * factor, base);
+      line(a.x, a.y, b.x, b.y);
+    }
+    text(base + ":" + factor, -r-5, -r-5);
+    increaseFactor();
+  } else if (factor > base) {
+    increaseBase();  
   }
+  factor = Math.round(factor * 100) / 100;
+  if (base > finalBase) {
+    noLoop();
+  }
+}
+
+function decreaseFactor() {
+  factor += -1;
+}
+
+function increaseFactor() {
+  factor += 1;
+}
+
+function decreaseBase() {
+  base += -1;
+  factor = minFactor;
+}
+
+function increaseBase() {
+  base += 1;
+  factor = minFactor;
 }
